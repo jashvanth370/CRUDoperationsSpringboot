9cb789dec7d760e3e2a280ca62d240f1f7ae8c2b
jashvanth370 <jashjazz11@gmail.com>
2025-03-21 15:03:52 +0530

    student Repository Create

--- src/main/java/com/example/CRUDoperationsSpringboot/service/StudentService.java
+++ src/main/java/com/example/CRUDoperationsSpringboot/service/StudentService.java
@@ -1,8 +1,31 @@
 package com.example.CRUDoperationsSpringboot.service;
 
+import com.example.CRUDoperationsSpringboot.model.Student;
+import com.example.CRUDoperationsSpringboot.repository.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StudentService {
 
+    @Autowired
+    StudentRepo studentRepo;
+
+    public List<Student> getAllStudent(){
+        return studentRepo.findAll();
+    }
+
+    public Student getSrudentById(int rollno){
+        return studentRepo.findById(rollno).get();
+    }
+
+    public Student updateStudentById(Student student){
+        return studentRepo.save(student);
+    }
+
+    public void deleteStudentById(int rollno) {
+        studentRepo.deleteById(rollno);
+    }
 }
