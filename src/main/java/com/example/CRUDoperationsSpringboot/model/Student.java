6323d675ca2e74fbf86fee8efcd039d9b5e0ebb3
jashvanth370 <jashjazz11@gmail.com>
2025-03-21 14:24:05 +0530

    Initial commit

---
+++ src/main/java/com/example/CRUDoperationsSpringboot/model/Student.java
@@ -0,0 +1,28 @@
+package com.example.CRUDoperationsSpringboot.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Student_Details")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Roll_Number")
+    private int rollno;
+
+    @Column(name = "Student_Name")
+    private String stuName;
+
+    @Column(name = "Gender")
+    private String gender;
+
+
+}
---
+++ src/test/java/com/example/CRUDoperationsSpringboot/CruDoperationsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUDoperationsSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruDoperationsSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
